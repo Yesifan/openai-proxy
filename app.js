834071b4f096b55ff6ad78ecafe95b1922713df8
叶思凡 <yesifan66@qq.com>
2023-11-08 15:37:52 +0800

    Update OPENAI_BASE_URL to use official API endpoint.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const Koa = require('koa');
 const { bodyParser } = require("@koa/bodyparser");
 
-const OPENAI_BASE_URL = process.env.OPENAI_BASE_URL ?? 'https://api.openai-proxy.com'
+const OPENAI_BASE_URL = process.env.OPENAI_BASE_URL ?? 'https://api.openai.com/v1'
 
 const API_URL_LIST = [
   '/models',
